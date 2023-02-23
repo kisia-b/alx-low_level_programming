73bbf2bb9271edc47f031207c72d05007d221da6
Brian Kisia <kisiab91@gmail.com>
2023-02-23 11:34:41 -0800

    updates

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,18 @@
  */
 void print_line(int n)
 {
-	int i;
+	int co;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (co = 1; co <= n; co++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
